e43ccc6ff838a1dbd8951c49fc06552f9f4a9ccf
Daniel Brotsky <dan@brotsky.com>
2025-11-04 22:18:29 -0800

    Fix Tsml output for Al-Anon.

--- src/app/Http/Resources/Query/TsmlMeetingResource.php
+++ src/app/Http/Resources/Query/TsmlMeetingResource.php
@@ -26,42 +26,70 @@ class TsmlMeetingResource extends JsonResource
         $formatsById = $this->formatsById;
         // Mapping formats to TSML types.
         // Servers may use arbitrary `key_string` values for formats, which are not guaranteed to be unique.
-        // To ensure consistency, we map format IDs to their `worldid_mixed` codes (which are unique and standardized),
-        // and then map those to TSML type codes using the table below.
-        // WORLD_ID -> TSML
+        // To help with consistency, we try to map format IDs to their `worldid_mixed` codes, which are
+        // standardized. If we find a standard code, we use it, otherwise we use the `key_string` value.
+        //
+        // All of the `worldid_mixed` codes have matching codes pre-defined in TSML for the Al-Anon program.
+        // (Well, not quite all, there is no code for sign language, but a bug has been filed.)
+        // The webmaster may have to define codes for any `key_string` values that get passed through.
+        //
+        // Note: We always pass the key_string for any `LANG` codes, because they are languages. The
+        // codes `HYBRID` and `VM` are pseudo-codes required by BMLT but have no TSML equivalents, because
+        // TSML makes this decision based on whether there's a physical address and a valid online link,
+        // so we ignore these two codes.
+        //
+        // This code has to be updated whenever the `worldid_mixed` values change. The code here is based
+        // on this dump of the Al-Anon WSO codes as of 2025-10-28:
+        /*
+        ```
+            +---------------+------------+--------------------+
+            | worldid_mixed | key_string | name_string        |
+            +---------------+------------+--------------------+
+            | AC            | AC         | Adult Children     |
+            | BEG           | B          | Beginners          |
+            | CC            | CC         | Child Care         |
+            | CLOSED        | C          | Closed             |
+            | FF            | FF         | Fragrance Free     |
+            | HYBR          | HY         | Hybrid Meeting     |
+            | LANG          | ENG        | English            |
+            | LANG          | ESP        | Español            |
+            | LANG          | FRA        | Français           |
+            | LGBT          | LGBT       | LGBTQIA+           |
+            | M             | M          | Men                |
+            | OPEN          | O          | Open               |
+            | P             | P          | Parents            |
+            | POC           | POC        | People of Color    |
+            | SL            | SL         | Sign Language      |
+            | SMOK          | SMOK       | Smoking Permitted  |
+            | TC            | TC         | Temporarily Closed |
+            | VM            | VM         | Virtual Meeting    |
+            | W             | W          | Women              |
+            | WCHR          | WC         | Handicap Access    |
+            | Y             | Y          | Alateen            |
+            | YA            | YA         | Young Adults       |
+            +---------------+------------+--------------------+
+         ```
+         */
         $worldIdToTsmlTypes = [
-            'CPT' => 'CPT',
-            'BT' => 'BT',
-            'BEG' => 'BEG',
-            'CAN' => 'CAN',
-            'CW' => 'CW',
+            'AC' => 'AC',
+            'BEG' => 'BE',
+            'CC' => 'BA',
             'CLOSED' => 'C',
-            'DISC' => 'DISC',
-            'GL' => 'GL',
-            'IP' => 'IP',
-            'IW' => 'IW',
-            'JFT' => 'JFT',
-            'LIT' => 'LIT',
-            'LC' => 'LC',
-            'TC' => 'TC',
+            'FF' => 'FF',
+            'HYBR' => '*ignore*',
+            'LANG' => '*use-key*',
+            'LGBT' => 'LGBTQIA',
             'M' => 'M',
-            'MED' => 'MED',
-            'NS' => 'NS',
-            'VM' => 'ONL',
             'OPEN' => 'O',
-            'QA' => 'QA',
-            'RA' => 'RA',
-            'SMOK' => 'SMOK',
-            'SPAD' => 'SPAD',
-            'SPK' => 'SPK',
-            'STEP' => 'STEP',
-            'SWG' => 'SWG',
-            'TOP' => 'TOP',
-            'TRAD' => 'TRAD',
-            'VAR' => 'VAR',
-            'WCHR' => 'X',
+            'P' => 'POA',
+            'POC' => 'POC',
+            'SL' => 'SL',
+            'SMOK' => 'SM',
+            'VM' => '*ignore*',
             'W' => 'W',
+            'WCHR' => 'X',
             'Y' => 'Y',
+            'YA' => 'YA',
         ];
 
         $data = $this->data->mapWithKeys(fn ($d) => [$d->key => $d->data_string])->toArray();
@@ -71,12 +99,40 @@ class TsmlMeetingResource extends JsonResource
         $meetingFormats = explode(',', $this->formats ?? '');
         $virtualOnly = in_array('VM', $meetingFormats) && !in_array('HY', $meetingFormats);
 
+        $getTsmlCode = function ($key) use ($formatsById, $worldIdToTsmlTypes) {
+            if (isset($formatsById[$key])) {
+                $worldId = $formatsById[$key]->worldid_mixed;
+                if (empty($worldId)) {
+                    return $formatsById[$key]->key_string;
+                }
+                $tsmlCode = $worldIdToTsmlTypes[$worldId];
+                if (empty($tsmlCode)) {
+                    return $formatsById[$key]->key_string;
+                } else if ($tsmlCode == '*ignore*') {
+                    return null;
+                } else if ($tsmlCode == '*use-key*') {
+                    return $formatsById[$key]->key_string;
+                } else {
+                    return $tsmlCode;
+                }
+            } else {
+                return null;
+            }
+        };
+
+        $computeNotes = function ($comments, $worldid_mixed) {
+            $prefix = empty($comments) ? '' : $comments;
+            $suffix = empty($worldid_mixed) ? '' : "WSO #" . $worldid_mixed;
+            return empty($prefix) ? $suffix : $prefix . "\n" . $suffix;
+        };
+
         return [
             'day' => isset($this->weekday_tinyint) ? intval($this->weekday_tinyint)  : null,
             'time' => $this->start_time ? substr($this->start_time, 0, 5) : null,
             'end_time' => (!empty($this->start_time) && !empty($this->duration_time)) ? TsmlMeetingResource::addTimes($this->start_time, $this->duration_time) : null,
             'name' => $allData['meeting_name'] ?? '',
             'location' => $virtualOnly ? 'Online Meeting' : ($allData['location_text'] ?? ''),
+            'location_notes' => $virtualOnly ? '' : ($allData['location_info'] ?? ''),
             'formatted_address' => collect(['location_street', 'location_municipality', 'location_province', 'location_postal_code_1', 'location_nation'])->map(fn($key) => $data[$key] ?? '')->filter()->implode(', '),
             'address' => $allData['location_street'] ?? '',
             'city' => $allData['location_municipality'] ?? '',
@@ -84,14 +140,11 @@ class TsmlMeetingResource extends JsonResource
             'postal_code' => $allData['location_postal_code_1'] ?? '',
             'country' => $allData['location_nation'] ?? '',
             'types' => collect($this->formats ? explode(',', $this->formats) : [])
-                ->map(fn($id) => intval($id))
-                ->filter(fn($id) => isset($formatsById[$id]) && !empty($formatsById[$id]->worldid_mixed))
-                ->map(fn($id) => $worldIdToTsmlTypes[$formatsById[$id]->worldid_mixed] ?? null)
+                ->map(fn($id) => $getTsmlCode(intval($id)))
                 ->filter()
                 ->values()
                 ->toArray(),
-            'location_notes' => $allData['location_info'] ?? '',
-            'notes' => $allData['comments'] ?? '',
+            'notes' => $computeNotes($allData['comments'] ?? '', $this->worldid_mixed ?? ''),
             'coordinates' =>  $this->venue_type == 2
                 ? null
                 : (($this->latitude && $this->longitude) ? "{$this->latitude},{$this->longitude}" : null),
